6d6afa64b6c8da3462c2cc6ebf1300aac8fbf044
Pelegram <pkptre@gmail.com>
2024-05-05 22:10:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_2VosC74Az', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '21j7v2ekc61jad9bcbq43l6hqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1"new_config"' // e.g. us-east-2
+        region: ''    //'us-east-1"new_config"' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://e1l8s3d0bf.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
